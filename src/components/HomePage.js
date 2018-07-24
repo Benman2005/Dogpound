6c71f169d46a24ec41fe479efcfd9841b8de90ee
Benjamin <benman2005@gMAIL.com>
2018-07-24 13:29:44 +0200

    added data

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,18 @@
 import React, { PureComponent } from 'react';
+import { Link } from 'react-router-dom'
 
 export default class StartPage extends PureComponent {
     render(){
 
         return (
         <div>
-            HOMEPAGE
+            Welcome to the Dogpound!
+            <div>
+            <Link to="/buy">Buy dog</Link>
+            </div>
+            <div>
+            <Link to="/sell">Sell dog</Link>
+            </div>
         </div>
         )
     }
---
+++ src/data/userData.js
@@ -0,0 +1,76 @@
+export const userData = [{
+    id: 1,
+    name: "Burt",
+    age: 12,
+    description: "I love long walks on the beach. Naked.",
+    photo: "image",
+    type: "buyer"
+},
+{
+    id: 2,
+    name: "Dick",
+    age: 34,
+    description: "I love dogs, like really love them. Naked.",
+    photo: "image",
+    type: "buyer"
+},
+{
+    id: 3,
+    name: "Melvin",
+    age: 56,
+    description: "Hearting dogs since my wife passed.",
+    photo: "image",
+    type: "buyer"
+},
+{
+    id: 4,
+    name: "Dwight",
+    age: 45,
+    description: "Got dogs?",
+    photo: "image",
+    type: "buyer"
+},
+{
+    id: 5,
+    name: "Tommy",
+    age: 8,
+    description: "some text some text some text some text",
+    photo: "image",
+    type: "seller"
+},
+{
+    id: 6,
+    name: "Rachel",
+    age: 2,
+    description: "some text some text some text some text",
+    photo: "image",
+    type: "seller"
+},
+{
+    id: 7,
+    name: "Dirk",
+    age: 11,
+    description: "some text some text some text some text",
+    photo: "img1",
+    type: "seller"
+
+},
+{
+    id: 8,
+    name: "Sean",
+    age: 13,
+    description: "some text some text some text some text",
+    photo: "img1",
+    type: "seller"
+
+},
+{
+    id: 9,
+    name: "Bart",
+    age: 99,
+    description: "some text some text some text some text",
+    photo: "img1",
+    type: "seller"
+
+}
+]
