7385d3695f43b64db4e3725a70073b4706e50a27
Benjamin <benman2005@gMAIL.com>
2018-07-24 16:17:30 +0200

    trying onclick

---
+++ src/actions/likeFunction.js
@@ -0,0 +1,11 @@
+import {buyers} from '../data/userData'
+
+export const LIKE_USER = 'LIKE_USER'
+
+export function likeUser(liked) {
+    return {
+      type: LIKE_USER,
+      payload: liked
+
+    }
+  }
