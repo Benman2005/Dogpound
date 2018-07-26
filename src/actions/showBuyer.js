e372b66c9c22496ecb71f75682bb00451afbfa0a
Benjamin <benman2005@gMAIL.com>
2018-07-26 13:39:02 +0200

    no same users showing! :)

--- src/actions/showBuyer.js
+++ src/actions/showBuyer.js
@@ -1,13 +1,35 @@
 
 import {buyers} from '../data/userData'
 
-export const SHOW_BUYER = 'SHOW_BUYER'
 
+export const SHOW_BUYER = 'SHOW_BUYER'
+export const SHOW_SELLER = 'SHOW_SELLER'
+
+var i = -1;
 
 export function showBuyer() {
+  function increment(n){
+    if (i === buyers.length-1){
+      window.alert("STOP! No more Dog lovers left..")
+    }
+    if (n<buyers.length-1) {
+    n++
+    return n
+    }
+   else 
+    return (n=buyers.length-1
+    )
+  }
+  i = increment(i)
+
+  if (i === buyers.length-1){
     return {
       type: SHOW_BUYER,
-      payload: buyers[Math.floor(Math.random() * buyers.length)]
-
-    }
+      payload: buyers[i]
+    }  
+  }
+  else return {
+      type: SHOW_BUYER,
+      payload: buyers[i]
+    }  
   }
